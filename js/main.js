e2cc721feb4a7e80962bb6994c3c1a21c69a398c
Zykkl <zykkl@pm.me>
2021-12-05 21:57:54 +0100

    responsive & more "modern" ig

--- js/main.js
+++ js/main.js
@@ -184,23 +184,31 @@ window.addEventListener('scroll', () => {
 	throttle(scrollProc, 50)
 })
 
-var acc = document.getElementsByClassName("accordion");
-var i;
+var acc = document.getElementsByClassName('accordion')
+var i
 
 for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    /* Toggle between adding and removing the "active" class,
+	acc[i].addEventListener('click', function () {
+		/* Toggle between adding and removing the "active" class,
     to highlight the button that controls the panel */
-    this.classList.toggle("active");
+		this.classList.toggle('active')
 
-    /* Toggle between hiding and showing the active panel */
-    var panel = this.nextElementSibling;
-    if (panel.style.display === "block") {
-      panel.style.display = "none";
-    } else {
-      panel.style.display = "block";
-    }
-  });
-} 
+		/* Toggle between hiding and showing the active panel */
+		var panel = this.nextElementSibling
+		if (panel.style.display === 'block') {
+			panel.style.display = 'none'
+		} else {
+			panel.style.display = 'block'
+		}
+	})
+}
+
+const faqs = document.querySelectorAll('.faq')
+
+faqs.forEach((faq) => {
+	faq.addEventListener('click', () => {
+		faq.classList.toggle('active')
+	})
+})
 
 //#endregion
