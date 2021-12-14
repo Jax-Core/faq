9b24f2ac5fbd29aa8ec2590b651b3c42194d29e3
Jax <80020581+EnhancedJax@users.noreply.github.com>
2021-12-14 18:52:06 +0800

    Update faq.js

--- data/faq.js
+++ data/faq.js
@@ -1,10 +1,22 @@
 const faqsData = `[
     {
-        "q": "How do I add shortcuts to ValliStart?",
-        "a": "[This](https://github.com/EnhancedJax/ValliStart#installation) guide will follow you through it."
+        "q": "Is JaxCore safe? Does it contain any viruses?",
+        "a": "No. JaxCore is fully open source, you can view it's source code on the [github repo](https://github.com/Jax-Core/JaxCore). The application runs entirely offline (apart from installation and updates)"
     },
     {
-        "q": "What if I don't know how to do this and that?",
-        "a": "Well you just gotta know it"
+        "q": "Is this really entirely free?",
+        "a": "JaxCore is made by me in my free time, the main goal is to make things easier for myself and the user. A lot of the productivity apps I found on the internet are either paid or uncustomizable, so I've made mine both free and customizable!"
+    },
+    {
+        "q": "What's Rainmeter, and why do I need it?",
+        "a": "Rainmeter is an open source program made to make creating desktop widgets and app easier. JaxCore runs entirely on Rainmeter, so without it JaxCore cannot be ran."
+    },
+    {
+        "q": "I've encountered a few bugs and issues, where can I report them?",
+        "a": "Join the Core Community Discord server [here](https://discord.gg/JmgehPSDD6), or simply create an issue on the github repo for the module that you're having trouble with!"
+    },
+    {
+        "q": "How can I support this project?",
+        "a": "Please consider donating from my [Ko-fi](https://ko-fi.com/jaxoriginals) page, this motivates me to keep going!"
     }
 ]`
