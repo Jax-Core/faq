58b44e9314a2a9c65e2877d6a0c80d27d4fe4b6b
Shaktijeet Sahoo <sahooshaktijeet@gmail.com>
2021-12-14 18:04:14 +0530

    Finishing off styling

--- data/faq.js
+++ data/faq.js
@@ -1,26 +1,31 @@
 const faqsData = `[
     {
-        "q": "Is JaxCore safe? Does it contain any viruses?",
-        "a": "No. JaxCore is fully open source, you can view it's source code on the [github repo](https://github.com/Jax-Core/JaxCore). The application runs entirely offline (apart from installation and updates)"
-    },
-    {
-        "q": "Is this really entirely free?",
-        "a": "JaxCore is made by me in my free time, the main goal is to make things easier for myself and the user. A lot of the productivity apps I found on the internet are either paid or uncustomizable, so I've made mine both free and customizable!"
-    },
-    {
-        "q": "What's Rainmeter, and why do I need it?",
-        "a": "Rainmeter is an open source program made to make creating desktop widgets and app easier. JaxCore runs entirely on Rainmeter, so without it JaxCore cannot be ran."
-    },
-    {
         "q": "How do I install / configure / uninstall JaxCore?",
         "a": "Please read the [JaxCore wiki](https://github.com/Jax-Core/JaxCore/wiki#installation)."
     },
+
+    {
+        "q": "What's Rainmeter, and why do I need it?",
+        "a": "Rainmeter is an open source program made to make creating desktop widgets and app easier. JaxCore runs entirely on Rainmeter."
+    },
+
+    {
+        "q": "Is JaxCore safe? Does it contain any viruses?",
+        "a": "No. JaxCore is open source, and uses only open source tools. You can get the source code from [GitHub](https://github.com/Jax-Core/JaxCore)."
+    },
+
+    {
+        "q": "Is JaxCore free?",
+        "a": "JaxCore is made by me in my free time, the main goal being making things easier for myself and others. A lot of the productivity apps I found on the internet are either paid or uncustomizable, so I decided to make Core both free and customizable!"
+    },
+    
     {
         "q": "I've encountered a few bugs and issues, where can I report them?",
-        "a": "Join the Core Community Discord server [here](https://discord.gg/JmgehPSDD6), or simply create an issue on the github repo for the module that you're having trouble with!"
+        "a": "Join the Core Community Discord server [here](https://discord.gg/JmgehPSDD6), or simply open an issue on the github repo for the module that you're having trouble with!"
     },
+
     {
         "q": "How can I support this project?",
-        "a": "Please consider donating from my [Ko-fi](https://ko-fi.com/jaxoriginals) page, this motivates me to keep going!"
+        "a": "Consider donating on my [Ko-fi](https://ko-fi.com/jaxoriginals) page, this motivates me to keep going!"
     }
 ]`
